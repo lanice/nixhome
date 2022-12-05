ed93e5be85a20f153413d26c0f4744483203fa66
Leander Neiß <1871704+lanice@users.noreply.github.com>
2022-12-05 19:02:16 +0100

    Add some zsh plugins.

--- home.nix
+++ home.nix
@@ -103,10 +103,25 @@ in {
 
   programs.zsh = {
     enable = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
 
     oh-my-zsh = {
       enable = true;
-      plugins = ["git"];
+      plugins = [
+        "colored-man-pages"
+        "command-not-found"
+        "docker"
+        "git"
+        "npm"
+        "pep8"
+        "pip"
+        "pyenv"
+        "python"
+        "sudo"
+        "systemd"
+        "ubuntu"
+      ];
       theme = "robbyrussell";
     };
   };
