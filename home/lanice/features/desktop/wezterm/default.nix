5b429c2f49dfdf1def09cb8a3d716a6e5d794082
Leander Neiß <1871704+lanice@users.noreply.github.com>
2023-03-04 11:28:18 +0100

    Add founderterms bash function to spawn wezterm panes for founderbuddies

--- home/lanice/features/desktop/wezterm/default.nix
+++ home/lanice/features/desktop/wezterm/default.nix
@@ -1,3 +1,14 @@
 {
   home.file.".config/wezterm/wezterm.lua".source = ./wezterm.lua;
+
+  programs.bash = {
+    initExtra = ''
+      founderterms () {
+        cd /home/lanice/dev/projects/founderbuddies
+        wezterm cli split-pane --right --cwd /home/lanice/dev/projects/founderbuddies
+        wezterm cli split-pane --bottom --cwd /home/lanice/dev/projects/founderbuddies
+        pscale connect founderbuddies dev --port 3309
+      }
+    '';
+  };
 }
