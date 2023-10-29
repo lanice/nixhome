9d30fc6ba070e58d65059f96a7603b0cd571ed63
Leander Neiß <1871704+lanice@users.noreply.github.com>
2023-10-29 19:04:00 +0100

    Use swaylock instead of swaylock-effects for now

--- home/lanice/features/desktop/common/wayland-wm/swaylock.nix
+++ home/lanice/features/desktop/common/wayland-wm/swaylock.nix
@@ -1,11 +1,13 @@
-{ config, pkgs, ... }:
-
-let inherit (config.colorscheme) colors;
-in
 {
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (config.colorscheme) colors;
+in {
   programs.swaylock = {
     enable = true;
-    package = pkgs.swaylock-effects;
+    # package = pkgs.swaylock-effects;
     settings = {
       effect-blur = "20x3";
       fade-in = 0.1;
