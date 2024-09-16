230145a3283ea38048f059f174fc15ef85f54a4f
Leander Neiss <1871704+lanice@users.noreply.github.com>
2024-09-16 15:32:04 -0400

    Update fabric-ai to v1.4.17

--- pkgs/fabric-ai/default.nix
+++ pkgs/fabric-ai/default.nix
@@ -3,31 +3,33 @@
   buildGoModule,
   fetchFromGitHub,
   nix-update-script,
-}:
-buildGoModule {
-  pname = "fabric-ai";
-  version = "1.4.14";
+}: let
+  version = "1.4.17";
+in
+  buildGoModule {
+    pname = "fabric-ai";
+    inherit version;
 
-  src = fetchFromGitHub {
-    owner = "danielmiessler";
-    repo = "fabric";
-    rev = "v1.4.14";
-    hash = "sha256-syQVGtEFCC7ZVcs6ZIJgH0EXItclV2rPO7xTfsG2g7Q=";
-  };
+    src = fetchFromGitHub {
+      owner = "danielmiessler";
+      repo = "fabric";
+      rev = "v${version}";
+      hash = "sha256-oH4HL2QQnX8f4w5YFP8XSQGg2tMkJex+mJHpu2Rtl8M=";
+    };
 
-  vendorHash = "sha256-CHgeHumWtNt8SrbzzCWqBdLxTmmyDD2bfLkriPeez2E=";
+    vendorHash = "sha256-CHgeHumWtNt8SrbzzCWqBdLxTmmyDD2bfLkriPeez2E=";
 
-  ldflags = [
-    "-s"
-    "-w"
-  ];
+    ldflags = [
+      "-s"
+      "-w"
+    ];
 
-  passthru.updateScript = nix-update-script {};
+    passthru.updateScript = nix-update-script {};
 
-  meta = {
-    description = "Fabric is an open-source framework for augmenting humans using AI. It provides a modular framework for solving specific problems using a crowdsourced set of AI prompts that can be used anywhere";
-    homepage = "https://github.com/danielmiessler/fabric";
-    license = lib.licenses.mit;
-    mainProgram = "fabric";
-  };
-}
+    meta = {
+      description = "Fabric is an open-source framework for augmenting humans using AI. It provides a modular framework for solving specific problems using a crowdsourced set of AI prompts that can be used anywhere";
+      homepage = "https://github.com/danielmiessler/fabric";
+      license = lib.licenses.mit;
+      mainProgram = "fabric";
+    };
+  }
