a28db84b31a332dc5ff7a7366273c562cda38fb3
Leander Neiss <1871704+lanice@users.noreply.github.com>
2025-03-22 11:56:16 -0400

    Use podman dockerCompat instead of docker. Fix distrobox default volume read-only mounting.

--- home/lanice/features/distrobox/default.nix
+++ home/lanice/features/distrobox/default.nix
@@ -3,6 +3,6 @@
 
   # https://github.com/nix-community/home-manager/issues/4430#issuecomment-1732681618
   home.file.".config/distrobox/distrobox.conf".text = ''
-    container_additional_volumes="/nix/store:/nix/store:r /etc/profiles/per-user:/etc/profiles/per-user:r"
+    container_additional_volumes="/nix/store:/nix/store:ro /etc/static/profiles/per-user:/etc/profiles/per-user:ro"
   '';
 }
--- hosts/common/virtualisation.nix
+++ hosts/common/virtualisation.nix
@@ -4,17 +4,18 @@
       enable = true;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
-      # dockerCompat = true;
+      dockerCompat = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
 
-    docker = {
-      enable = true;
-    };
+    # docker = {
+    #   enable = true;
+    # };
   };
-  environment.systemPackages = with pkgs; [
-    docker-compose
-  ];
+
+  # environment.systemPackages = with pkgs; [
+  #   docker-compose
+  # ];
 }
