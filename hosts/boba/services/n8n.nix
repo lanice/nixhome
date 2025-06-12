63da90c9416b62396d291004b29be21811663149
Leander Neiss <1871704+lanice@users.noreply.github.com>
2025-06-12 10:46:38 -0400

    boba: Specify n8n webhook url

--- hosts/boba/services/n8n.nix
+++ hosts/boba/services/n8n.nix
@@ -1,5 +1,8 @@
 {
-  services.n8n.enable = true;
+  services.n8n = {
+    enable = true;
+    webhookUrl = "https://n8n.lanice.dev/";
+  };
 
   systemd.services.n8n.environment.N8N_PORT = "5588";
 }
