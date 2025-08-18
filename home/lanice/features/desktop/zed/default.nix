6c5d7c0ba27d1826f7ceed34d0939f750221c7e2
Leander Neiss <1871704+lanice@users.noreply.github.com>
2025-08-18 17:37:36 -0400

    Add tailwind CSS zed theme [WIP]

--- home/lanice/features/desktop/zed/default.nix
+++ home/lanice/features/desktop/zed/default.nix
@@ -1,5 +1,12 @@
-{
-  programs.zed-editor = {
+{lib, ...}: {
+  programs.zed-editor = let
+    themeData = builtins.fromJSON (builtins.readFile ./tailwind-css-theme.json);
+  in {
     enable = true;
+    themes = builtins.listToAttrs (map (theme: {
+        name = builtins.replaceStrings [" " "(" ")"] ["-" "" ""] (lib.strings.toLower theme.name);
+        value = theme;
+      })
+      themeData.themes);
   };
 }
