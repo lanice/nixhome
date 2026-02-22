c2279e06a55b7758f74fb4b0819472596e4371a7
Leander Neiss <1871704+lanice@users.noreply.github.com>
2026-02-22 12:08:49 -0500

    Consolidate

--- home/lanice/themes/catppuccin-frappe/default.nix
+++ home/lanice/themes/catppuccin-frappe/default.nix
@@ -10,13 +10,6 @@
     userSettings."workbench.iconTheme" = "catppuccin-frappe";
   };
 
-  programs.btop.settings = {
-    color_theme = lib.mkForce "catppuccin-frappe";
-  };
+  programs.btop.settings = color_theme = lib.mkForce "catppuccin-frappe";
   home.file.".config/btop/themes/catppuccin-frappe.theme".source = ./btop-catppuccin-frappe.theme;
-
-  programs.rofi.theme = builtins.fetchurl {
-    url = "https://raw.githubusercontent.com/catppuccin/rofi/main/basic/.local/share/rofi/themes/catppuccin-frappe.rasi";
-    sha256 = "sha256:0zvb8v10alypwzd8i2r8izy4vy150jxnnv3frvvhwxsvm8d7lifm";
-  };
 }
--- home/lanice/themes/catppuccin-latte/default.nix
+++ home/lanice/themes/catppuccin-latte/default.nix
@@ -12,31 +12,4 @@
 
   programs.btop.settings.color_theme = lib.mkForce "catppuccin-latte";
   home.file.".config/btop/themes/catppuccin-latte.theme".source = ./btop-catppuccin-latte.theme;
-
-  programs.rofi.theme = builtins.fetchurl {
-    url = "https://raw.githubusercontent.com/catppuccin/rofi/main/basic/.local/share/rofi/themes/catppuccin-latte.rasi";
-    sha256 = "sha256:1qkb19lpl2qpfvxyyw0rp05k92al2m7s59rpxpmmf99rblgs9313";
-  };
-
-  # Seems not to work atm
-  # programs.discocss = {
-  #   enable = true;
-
-  #   package = pkgs.discocss.overrideAttrs (old: rec {
-  #     version = "0.3.1";
-  #     src = pkgs.fetchFromGitHub {
-  #       owner = "bddvlpr";
-  #       repo = "discocss";
-  #       rev = "v${version}";
-  #       hash = "sha256-BFTxgUy2H/T92XikCsUMQ4arPbxf/7a7JPRewGqvqZQ=";
-  #     };
-  #   });
-  #   # discord = pkgs.discord.override {withOpenASAR = true;};
-  #   discordPackage = pkgs.discord.override {withVencord = true;};
-
-  #   css = builtins.readFile (builtins.fetchurl {
-  #     url = "https://catppuccin.github.io/discord/dist/catppuccin-latte.theme.css";
-  #     sha256 = "sha256:029lx97181gs69r0g9amxcpnvsyixxqsf3fxi2vmnabriz03ji10";
-  #   });
-  # };
 }
