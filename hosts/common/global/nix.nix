1740131fe07003e5004b9742bb7974e5640e45c1
Leander Neiss <1871704+lanice@users.noreply.github.com>
2026-03-09 19:33:33 -0400

    Add nixos-cuda binary cache

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -11,8 +11,8 @@
       flake-registry = ""; # Disable global flake registry
 
       trusted-substituters = ["https://devenv.cachix.org/"];
-      extra-substituters = ["https://devenv.cachix.org/" "https://cache.numtide.com"];
-      extra-trusted-public-keys = ["devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="];
+      extra-substituters = ["https://devenv.cachix.org/" "https://cache.numtide.com" "https://cache.nixos-cuda.org"];
+      extra-trusted-public-keys = ["devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="];
     };
     gc = {
       automatic = true;
