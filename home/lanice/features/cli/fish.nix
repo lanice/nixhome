9969a0b86f862c89a27a038db6936c4fc712c2a0
Leander Neiss <1871704+lanice@users.noreply.github.com>
2026-03-11 15:57:44 -0400

    Clean up fish config

--- home/lanice/features/cli/fish.nix
+++ home/lanice/features/cli/fish.nix
@@ -7,8 +7,6 @@
     enable = true;
 
     shellAbbrs = {
-      aptgrade = "sudo apt update && sudo apt upgrade";
-
       cat = "bat";
       df = "duf";
 
@@ -41,6 +39,7 @@
       gpr = "git pull --rebase";
       gw = "git worktree";
       gwa = "git worktree add";
+      gbd = "git branch -D";
 
       # eza
       ls = "eza";
@@ -84,35 +83,13 @@
         source ${config.home.homeDirectory}/.env.fish
       end
 
-      set -U fish_features qmark-noglob
-
-      # Use terminal colors
-      set -U fish_color_autosuggestion      brblack
-      set -U fish_color_cancel              -r
-      set -U fish_color_command             brgreen
-      set -U fish_color_comment             brmagenta
-      set -U fish_color_cwd                 green
-      set -U fish_color_cwd_root            red
-      set -U fish_color_end                 brmagenta
-      set -U fish_color_error               brred
-      set -U fish_color_escape              brcyan
-      set -U fish_color_history_current     --bold
-      set -U fish_color_host                normal
-      set -U fish_color_match               --background=brblue
-      set -U fish_color_normal              normal
-      set -U fish_color_operator            cyan
-      set -U fish_color_param               brblue
-      set -U fish_color_quote               yellow
-      set -U fish_color_redirection         bryellow
-      set -U fish_color_search_match        'bryellow' '--background=brblack'
-      set -U fish_color_selection           'white' '--bold' '--background=brblack'
-      set -U fish_color_status              red
-      set -U fish_color_user                brgreen
-      set -U fish_color_valid_path          --underline
-      set -U fish_pager_color_completion    normal
-      set -U fish_pager_color_description   yellow
-      set -U fish_pager_color_prefix        'white' '--bold' '--underline'
-      set -U fish_pager_color_progress      'brwhite' '--background=cyan'
+      # Custom fish color overrides
+      set -g fish_color_command             brgreen              # default: normal
+      set -g fish_color_comment             brmagenta            # default: red
+      set -g fish_color_end                 brmagenta            # default: green
+      set -g fish_color_match               --background=brblue  # default: unset
+      set -g fish_color_operator            cyan                 # default: brcyan
+      set -g fish_color_param               brblue               # default: cyan
     '';
 
     plugins = [
