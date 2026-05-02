4d0cce9d7f0d8ec7a8896358090b3592de6c7cba
Leander Neiss <1871704+lanice@users.noreply.github.com>
2026-05-02 13:57:24 -0400

    Update zed configs

--- home/lanice/features/desktop/zed/default.nix
+++ home/lanice/features/desktop/zed/default.nix
@@ -1,14 +1,60 @@
-{lib, ...}: {
-  programs.zed-editor = let
-    themeData = builtins.fromJSON (builtins.readFile ./tailwind-css-theme.json);
-  in {
-    enable = true;
-    themes = builtins.listToAttrs (map (theme: {
-        name = builtins.replaceStrings [" " "(" ")"] ["-" "" ""] (lib.strings.toLower theme.name);
-        value = theme;
-      })
-      themeData.themes);
+{pkgs, ...}: {
+  home.packages = [pkgs.nixd pkgs.alejandra];
 
-    extensions = ["catppuccin" "catppuccin-icons"];
+  programs.zed-editor = {
+    enable = true;
+
+    extensions = ["nix" "catppuccin" "catppuccin-icons"];
+
+    mutableUserSettings = true;
+    mutableUserKeymaps = true;
+
+    userSettings = {
+      ui_font_size = 18;
+      buffer_font_size = 18;
+
+      autosave.after_delay.milliseconds = 1000;
+
+      base_keymap = "VSCode";
+      show_whitespaces = "all";
+
+      languages.Nix = {
+        language_servers = ["nixd" "!nil"];
+        formatter = {
+          external = {
+            command = "alejandra";
+            arguments = ["-q"];
+          };
+        };
+      };
+    };
+
+    userKeymaps = [
+      {
+        context = "Editor && mode == full";
+        unbind = {
+          ctrl-alt-shift-down = "editor::DuplicateLineDown";
+          ctrl-alt-shift-up = "editor::DuplicateLineUp";
+
+          alt-down = "editor::MoveLineDown";
+          alt-up = "editor::MoveLineUp";
+        };
+        bindings = {
+          ctrl-enter = "editor::NewlineBelow";
+          ctrl-shift-enter = "editor::NewlineAbove";
+
+          ctrl-shift-down = "editor::MoveLineDown";
+          ctrl-shift-up = "editor::MoveLineUp";
+
+          ctrl-shift-d = "editor::DuplicateLineDown";
+        };
+      }
+      {
+        context = "!AcpThread > Editor && mode == full";
+        unbind = {
+          ctrl-enter = "assistant::InlineAssist";
+        };
+      }
+    ];
   };
 }
