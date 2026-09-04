5648f27472c5df354d238c0abb907cf14a5a72bf
Leander Neiss <1871704+lanice@users.noreply.github.com>
2026-09-03 22:24:21 -0400

    paperless workaround

--- hosts/boba/services/paperless.nix
+++ hosts/boba/services/paperless.nix
@@ -1,6 +1,27 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   services.paperless = {
     enable = true;
+    # fromtimestamp() otherwise interprets UTC sandbox time as Paperless local time.
+    package = pkgs.paperless-ngx.overrideAttrs (old: {
+      postPatch =
+        (old.postPatch or "")
+        + ''
+          substituteInPlace src/documents/consumer.py \
+            --replace-fail \
+              '            create_date = timezone.make_aware(
+                          datetime.datetime.fromtimestamp(stats.st_mtime),
+                      )' \
+              '            create_date = datetime.datetime.fromtimestamp(
+                          stats.st_mtime,
+                          tz=timezone.get_current_timezone(),
+                      )'
+        '';
+    });
+
     address = "0.0.0.0";
     port = 58080;
 
