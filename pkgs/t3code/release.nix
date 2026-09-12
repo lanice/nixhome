18b4b06338c81062220ff5fb2dfab1c386005530
Leander Neiss <1871704+lanice@users.noreply.github.com>
2026-09-12 02:02:04 -0400

    Upgrade t3code

--- pkgs/t3code/release.nix
+++ pkgs/t3code/release.nix
@@ -1,6 +1,6 @@
 {
-  version = "0.0.39";
-  desktopHash = "sha256-T3jJk5wDhvm+Wi2Om6+WRK9Jc0QBWBejb+qL6EmBvag=";
-  serverHash = "sha256-D/15Aah3NNKk8IP5JMpP7QjIF3BPnuny2HnKRgnDbJI=";
-  npmDepsHash = "sha256-AobsU1596f8jW06lac9+rVOW7jw8bnXmgCtUSOKLzFY=";
+  version = "0.0.40";
+  desktopHash = "sha256-i/X9RMt/rQxDGR1U/v35dKgifVBQXsuKvPdjJiCfJko=";
+  serverHash = "sha256-UaJMLPWTPGlkzNXysroS0HHKViyAP85G7wo0/gd29Cc=";
+  npmDepsHash = "sha256-KqV3zRmmvLdiMj5S7hfVLh1Ja5Tzkfiq+yBWS8HLCNw=";
 }
